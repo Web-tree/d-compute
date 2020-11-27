e4e397024ec4121a705a10356a6fec32e17e6d52
Max Levitskiy <Maxim.Levitskiy@pega.com>
2020-11-27 16:48:24 +0100

    tst

--- tst/tst.go
+++ tst/tst.go
@@ -6,32 +6,59 @@ import (
 	"os"
 )
 
-var maxChunkSize int64 = 10 * 1024 * 1024
+const maxChunkSize int64 = 65535
 
 type Schema struct {
 }
 
-func Disasemble(file *os.File) Schema {
+func Disasemble(seed int64, file *os.File) []byte {
 	stat, err := file.Stat()
 	if err != nil {
 		log.Panic(err)
 	}
 
 	size := stat.Size()
+	b := make([]byte, size)
 	if size < maxChunkSize {
-		b := make([]byte, size)
 		file.Read(b)
 		println(string(b))
 
-		r := rand.New(rand.NewSource(112))
-		r.Shuffle(int(size), func(i, j int) { b[i], b[j] = b[j], b[i] })
-		println(string(b))
+		r := rand.New(rand.NewSource(seed))
+
+		shuffleBytes(&b, randNumbers(r, int(size)))
 	}
-	return Schema{}
+	return b
 }
 
-func Assemble(schema Schema) {
+func shuffleBytes(b *[]byte, numbers []uint16) {
+	for i, j := range numbers {
+		(*b)[i], (*b)[j] = (*b)[j], (*b)[i]
+	}
+}
+func reverseShuffle(b *[]byte, numbers []uint16) {
+	for i := len(numbers) - 1; i >= 0; i-- {
+		j := numbers[i]
+		(*b)[i], (*b)[j] = (*b)[j], (*b)[i]
+	}
+}
 
+func randNumbers(r *rand.Rand, size int) []uint16 {
+	min := 0
+	max := size
+	result := make([]uint16, size)
+	for i := 0; i < max; i++ {
+		result[i] = uint16(r.Intn(max-min) + min)
+	}
+	return result
+}
+
+func Assemble(seed int64, bytes []byte) []byte {
+	r := rand.New(rand.NewSource(seed))
+	numbers := randNumbers(r, len(bytes))
+	reverseShuffle(&bytes, numbers)
+	//reverse(numbers)
+	//shuffleBytes(&bytes, numbers)
+	return bytes
 }
 
 func main() {
@@ -39,5 +66,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	Disasemble(file)
+	seed := int64(111)
+
+	println(string(Assemble(seed, Disasemble(seed, file))))
+}
+
+func reverse(numbers []uint16) {
+	for i := 0; i < len(numbers)/2; i++ {
+		j := len(numbers) - i - 1
+		numbers[i], numbers[j] = numbers[j], numbers[i]
+	}
 }
